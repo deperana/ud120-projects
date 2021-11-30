168887d87df284d31ecb70b007b4953c7f290f53
deperana <48857935+deperana@users.noreply.github.com>
2021-11-29 23:09:37 -0500

    Update svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -22,7 +22,10 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+Clf.fit (features_train, labels_train)
 
+pred = clf.predict(features_test)
 
 #########################################################
 
